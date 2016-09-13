47baaac27bf8b96bc2c8f9a1c69e760059f91a05
John Kayne <johnhoganuke2@gmail.com>
2016-09-13 08:30:59 -0400

    Update PhysicsObject.java

--- JoGEn-RG-5/JoGEn-1/src/main/java/jogen/physics/world/object/PhysicsObject.java
+++ JoGEn-RG-5/JoGEn-1/src/main/java/jogen/physics/world/object/PhysicsObject.java
@@ -1,1 +1,14 @@
+package jogen.physics.world.object;
 
+public class PhysicsObject extends GraphicsObject {
+  public Position position;
+  public Rotation rotation;
+  public Velocity velocity;
+  
+  public float getVelocity() {
+    return this.velocity.getAverage();
+  }
+  public static float getVelocity(PhysicsObject object) {
+    return object.getVelocity();
+  }
+}
